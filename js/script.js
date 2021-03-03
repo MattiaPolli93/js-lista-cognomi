7bfa2a73381a8d51f871549ae4e9dcdb333ce2f9
Mattia <matpolli93@gmail.com>
2021-03-03 15:58:20 +0100

    Code improved

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // Ask the user their surname, then place it in an array with other surnames; print the list alphabetically and state its place in the list.
 // Array and Variables
 var surnames = ["Bianchi", "Rossi", "Duzioni", "Franchi", "Martini", "Balsano", "Verdi"];
-var userSurname = prompt("Hello! Please insert your surname");
+
+do {
+    // Asking user's surname
+    var userSurname = (prompt("Hello! Please insert your surname")).toUpperCase();
+} while (!isNaN(userSurname));
 
 // Adding user's inserted surname inside the array
 surnames.push(userSurname);
